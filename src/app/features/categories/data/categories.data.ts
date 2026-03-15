5f7977d694f82142812780b8d5103d2687b4b4ef
javub25 <javub25@users.noreply.github.com>
2026-03-15 19:52:44 +0100

    feat(categories): add static categories data

---
+++ src/app/features/categories/data/categories.data.ts
@@ -0,0 +1,29 @@
+import { Categories } from "@/app/features/categories/models/categories.model";
+
+export const CATEGORIES_DATA : Categories = [
+    {
+        name: "education",
+        id: "c7e0b414-510e-4c9e-afad-b456c33034d0",
+        icon: "/assets/images/svg/educationIcon.svg",
+    },
+    {
+        name: "work",
+        id: "06e9e9d2-87a9-447e-be15-f2ea6c70edb0",
+        icon:"/assets/images/svg/workIcon.svg"
+    },
+    {
+        name: "health",
+        id: "c3f6c31b-6d16-4d22-86b5-f65fa73ee10e",
+        icon:"/assets/images/svg/medicineIcon.svg"
+    },
+    {
+        name: "art",
+        id: "2885b2e3-dac6-44ec-a35b-2587aa95e349",
+        icon: "/assets/images/svg/artIcon.svg"
+    },
+    {
+        name: "life",
+        id: "87dea9df-4027-4a10-8ca0-692f130c9850",
+        icon: "/assets/images/svg/lifeIcon.svg"
+    },
+]
